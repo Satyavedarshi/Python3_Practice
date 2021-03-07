807fe2de1bf8fda33b6c6437dc68c7015975645a
saramise <satyavedarshi.ramisetti@oracle.com>
2021-03-08 03:45:54 +0530

    added Strings tasks

--- basics/Strings_1.py
+++ basics/Strings_1.py
@@ -1,5 +1,6 @@
 import re, string, random
 from collections import Counter, OrderedDict
+from itertools import permutations
 
 #Python program to check if a string is palindrome or not
 #Using Loops
@@ -97,7 +98,7 @@ def uncommon_strings(str1,str2):
     return set(str1.split()).symmetric_difference(set(str2.split()))
 
 #Python – Replace duplicate Occurrence in String
-def rep_dup(test_str):
+def rep_dup_1(test_str):
     replace_strings = {'Gfg' :  'It', 'Classes' : 'They' }
     str_list = test_str.split()
     for items in replace_strings.items():
@@ -112,7 +113,55 @@ def rep_dup(test_str):
                     str_list[i] = items[1]
     print(' '.join(str_list))
 
+def rep_dup_2(test_str):
+    replace_strings = {'Gfg': 'It', 'Classes': 'They'}
+    str_list = test_str.split()
+    res = set()
+    for i in enumerate(str_list):
+        print(i)
+        if i[1] in replace_strings:
+            if i[1] not in res:
+                res.add(i[1])
+            else:
+                str_list[i[0]] = replace_strings[i[1]]
+    return ' '.join(str_list)
 
+def rep_dup_3(test_str):
+    word_list = ["best", 'CS', 'for']
+    for i in word_list:
+        if i in test_str:
+            test_str = re.sub(i,'gfg',test_str)
+    return test_str
+
+#Python | Permutation of a given string using inbuilt function
+def perm_str(test_str):
+    print(*[''.join(i) for i in permutations(test_str)],sep='\n')
+
+
+#Check for URL in a String
+def check_url(test_str):
+    urls = re.findall(re.compile(url_regex),test_str)
+    print(urls)
+
+#Execute a String of Code in Python
+def exec_str(test_str):
+    exec(test_str)
+
+#String slicing in Python to check if a string can become empty by recursive deletion
+def rec_del(test_str,del_str):
+    while(len(test_str)>0):
+        index1 = test_str.find(del_str)
+        if index1 != -1:
+            test_str = test_str[0:index1] + test_str[index1+len(del_str):]
+        else:
+            return False
+    return True
+
+#Replace all occurrences of a substring in a string
+def replac_all(test_str,sub_str,rep_str):
+    replace_str = str.maketrans(sub_str,rep_str)
+    test_str = test_str.translate(replace_str)
+    return test_str
 
 print(check_pallind_1('malayalam'), check_pallind_1('malayala'))
 
@@ -148,5 +197,19 @@ print(check_bin('0001010'))
 print(uncommon_strings('Geeks for Geeks','Learning from Geeks for Geeks'))
 
 
-print(rep_dup('Gfg is best . Gfg also has Classes now. Classes help understand better . '))
+print(rep_dup_1('Gfg is best . Gfg also has Classes now. Classes help understand better . '))
+print(rep_dup_2('Gfg is best . Gfg also has Classes now. Classes help understand better . '))
+print(rep_dup_3('Geeksforgeeks is best for geeks and CS'))
 
+print(perm_str('ABC'))
+
+#print(check_url('My Profile: https://auth.geeksforgeeks.org/user/Chinmoy%20Lenka/articles \
+#in the portal of http://www.geeksforgeeks.org/'))
+
+
+exec_str('a=6+5\nprint("the sum is ",a)')
+
+print(rec_del('GEEGEEKSKS','GEEKS'))
+print(rec_del('GEEGEEKSKES','GEEKS'))
+
+print(replac_all('geeksforgeeks','geek','abcd'))
